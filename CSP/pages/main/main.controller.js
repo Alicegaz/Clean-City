616cd09150b8235df7c762ce24b11f76cec62724
Andrey Rechitsky <arechitsky@gmail.com>
2017-07-17 17:45:41 +0700

    added GoogleMap

--- CSP/pages/main/main.controller.js
+++ CSP/pages/main/main.controller.js
@@ -1,79 +1,75 @@
 controllersModule.controller('mainController', function ($scope, $compile) {
-  var map;
-  var infoWindow;
-  function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
     });
-    var infoWindow = new google.maps.InfoWindow({map: map});
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
+  var triangleCoords = [
+  {lat: 25.774, lng: -80.190},
+  {lat: 18.466, lng: -66.118},
+  {lat: 32.321, lng: -64.757}
+  ];
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
+  var triangle1Coords = [
+  {lat: 35.774, lng: -60.190},
+  {lat: 18.466, lng: -66.118},
+  {lat: 32.321, lng: -64.757}
+  ];
+  new google.maps.Polygon({
+    map: map,
+    paths: triangleCoords,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 3,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+  new google.maps.Polygon({
+    map: map,
+    paths: triangle1Coords,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 3,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
 
-        map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
+  bermudaTriangle.setMap(map);
 
-    var triangleCoords = [
-    {lat: 25.774, lng: -80.190},
-    {lat: 18.466, lng: -66.118},
-    {lat: 32.321, lng: -64.757}
-    ];
+  // Add a listener for the click event.
+  bermudaTriangle.addListener('click', showArrays);
 
-    var triangle1Coords = [
-    {lat: 35.774, lng: -60.190},
-    {lat: 18.466, lng: -66.118},
-    {lat: 32.321, lng: -64.757}
-    ];
-    new google.maps.Polygon({
-      map: map,
-      paths: triangleCoords,
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 3,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35
-    });
-    new google.maps.Polygon({
-      map: map,
-      paths: triangle1Coords,
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 3,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35
-    });
+  infoWindow = new google.maps.InfoWindow;
 
-    bermudaTriangle.setMap(map);
-
-        // Add a listener for the click event.
-        bermudaTriangle.addListener('click', showArrays);
-
-        infoWindow = new google.maps.InfoWindow;
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-          'Error: The Geolocation service failed.' :
-          'Error: Your browser doesn\'t support geolocation.');
-      }
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+  }
 
 
-      /** @this {google.maps.Polygon} */
-      function showArrays(event) {
+  /** @this {google.maps.Polygon} */
+  function showArrays(event) {
         // Since this polygon has only one path, we can call getPath() to return the
         // MVCArray of LatLngs.
         var vertices = this.getPath();
