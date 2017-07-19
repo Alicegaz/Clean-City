5b20eea3b4a787981030c760f7e4cf0b33488aba
Syomina Svetlana <sem-sveta-ina@mail.ru>
2017-07-19 16:05:53 +0800

    Added InfoWindow

--- CSP/pages/main/main.controller.js
+++ CSP/pages/main/main.controller.js
@@ -1,6 +1,6 @@
 controllersModule.controller('mainController', function ($scope, $compile, AreaSrvc) {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 56, lng: 92},
     zoom: 8
   });
   AreaSrvc.getAll().then(
@@ -13,7 +13,7 @@ controllersModule.controller('mainController', function ($scope, $compile, AreaS
 		     	coords.push({lat: area.geoPoints[j].latitude, lng: area.geoPoints[j].longitude})
 		    }
 		    console.log(coords);
-	      	var area = new google.maps.Polygon({
+	      	var polygon = new google.maps.Polygon({
     			map: map,
     			paths: coords,
     			strokeColor: '#FF0000',
@@ -22,80 +22,20 @@ controllersModule.controller('mainController', function ($scope, $compile, AreaS
     			fillColor: '#FF0000',
    				fillOpacity: 0.35
              });
+        var contentString = 'Наименование организации: ' + area.responsible.name + '<br/>' +' Телефон: '+ area.responsible.phone + '<br/>'+ 'E-mail: ' + 
+        area.responsible.email
+		var infowindow = new google.maps.InfoWindow({
+			 content: contentString
+		});
+		polygon.addListener ('click', function(event){
+			infowindow.open(map);
+			infowindow.setPosition(event.latLng);
+			
+		});
+		   		 
 		  }
+		  		
+		  
 	  });
-  var infoWindow = new google.maps.InfoWindow({map: map});
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-
-  var Coords = [
-  {lat: 25.774, lng: -80.190},
-  {lat: 18.466, lng: -66.118},
-  {lat: 32.321, lng: -64.757}
-  ];
-
-  
-  var Area = new google.maps.Polygon({
-    map: map,
-    paths: Coords,
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 3,
-    fillColor: '#FF0000',
-    fillOpacity: 0.35
-  });
-  
-  Area.setMap(map);
-
-  // Add a listener for the click event.
-  Area.addListener('click', showArrays);
-
-  infoWindow = new google.maps.InfoWindow;
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.');
-  }
-
-
-  /** @this {google.maps.Polygon} */
-  function showArrays(event) {
-        var vertices = this.getPath();
-        var contentString = '<b>Bermuda Triangle polygon</b><br>' +
-        'Clicked location: <br>' + event.latLng.lat() + ',' + event.latLng.lng() +
-        '<br>';
-
-        // Iterate over the vertices.
-        /*for (var i =0; i < vertices.getLength(); i++) {
-          var xy = vertices.getAt(i);
-          contentString += '<br>' + 'Coordinate ' + i + ':<br>' + xy.lat() + ',' +
-          xy.lng();
-        } */
-
-       // Replace the info window's content and position.
-        infoWindow.setContent(contentString);
-        infoWindow.setPosition(event.latLng);
-
-        infoWindow.open(map);
-      }
-
-
+	 
     })
